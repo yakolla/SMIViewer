c99f29148885eb8f0703331bd2b8f856f207974d
yakolla <yakolla@gmail.com>
2017-01-24 17:57:00 +0900

    dd

---
+++ background.js
@@ -0,0 +1,120 @@
+﻿var onOff = true;
+var title = "Cool-ToolTip Translate";
+// Fix up prefixing
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext = new AudioContext();
+var audioSource = null;
+// Set up context menu at install time.
+chrome.runtime.onInstalled.addListener(
+    function () {
+       
+       chrome.contextMenus.create({
+            title: title + " '%s'",
+            contexts: ["selection"],
+            id: "2"
+        });  
+    }    
+);
+
+//var contents_port;
+//chrome.runtime.onConnect.addListener(function (port) {
+//    contents_port = port;
+//    console.debug(port);
+//})
+
+chrome.runtime.onMessage.addListener(
+function (request, sender, sendResponse) {
+    if (request.url != null) {
+        
+      //  $.get(request.url, function (data) {
+      //     sendResponse(data);  // 응답을 보냄    
+      //  }).fail(function () {
+      //  });
+        
+       
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", request.url, false);
+        xmlHttp.send();
+        sendResponse( xmlHttp.responseText);
+        return true;
+    }
+
+    if (request.soundUrl != null) {
+
+        function play(audioBuffer) {
+            if (audioSource != null) {
+                audioSource.stop();
+                audioSource = null;
+            }
+            
+            audioSource = audioContext.createBufferSource();
+            audioSource.buffer = audioBuffer;
+            audioSource.connect(audioContext.destination);
+            audioSource.start(0);
+        }
+
+        function loadSound(url) {
+            var request = new XMLHttpRequest();
+            request.open('GET', url, true);
+            request.responseType = 'arraybuffer';
+
+            // Decode asynchronously
+            request.onload = function () {
+                if (request.status == 200) {
+                    audioContext.decodeAudioData(request.response, play);
+                }
+            }
+            request.send();
+        }
+
+        loadSound(request.soundUrl);
+        return true;
+    }
+
+    sendResponse("");
+   
+    return true;
+})
+
+// add click event
+
+chrome.contextMenus.onClicked.addListener(
+    function (info, tab) {
+       
+        if (info.menuItemId == "1") {
+            
+            translateOnOff(info, tab);
+        }
+        else if (info.menuItemId == "2") {
+            translateSelectedText(info, tab);
+        }
+        
+    }
+);
+
+function translateOnOff(info, tab) {
+    
+    onOff = !onOff;
+       
+    chrome.contextMenus.update(info.menuItemId, {
+        title: title + " " + (onOff == true ? "On" : "Off")
+    });
+    
+    contents_port.postMessage({ id: 2, on: onOff });
+    
+}
+
+function translateSelectedText(info, tab) {
+    var sText = info.selectionText;
+    //contents_port.postMessage({ id: 1, greeting: sText });
+    doInCurrentTab(function (tab) {
+        chrome.tabs.sendMessage(tab.id, { id: 1, greeting: sText }, function (response) { });
+    });
+}
+
+function doInCurrentTab(tabCallback) {
+    chrome.tabs.query(
+        { currentWindow: true, active: true },
+        function (tabArray) { tabCallback(tabArray[0]); }
+    );
+}
---
+++ contents.js
@@ -0,0 +1,113 @@
+﻿
+var layerCount = 0;
+var layers = {};
+$(document).ready(function () {
+
+    loadOptions();
+
+    
+    var str = $("body pre").text();
+
+    var info = getNextSMIText(str, 0, str.length);    
+    lazyLoading(str, info);
+    $(document).mousedown(function (e) {
+        
+        return true;
+    });
+});
+
+function lazyLoading(str, info)
+{
+    setTimeout(function () {
+        if (info != null) {
+            var smiLayer = layers[info.langType];
+            if (null == smiLayer) {
+                smiLayer = createSMILayer(100, 250 * layerCount);
+                layers[info.langType] = smiLayer;
+                ++layerCount;
+            }
+
+            smiLayer.innerHTML += "<p>" + info.smiText + "</p>";
+            info = getNextSMIText(str, info.posNextSyncStartTag, str.length);
+            lazyLoading(str, info);
+        }
+    }, 1);
+}
+
+function createSMILayer(x, y) {
+
+    var myLayer = document.createElement('div');
+    myLayer.id = 'smiLayer';
+    document.body.appendChild(myLayer);
+
+    var myLayerContents = document.createElement('div');
+    myLayerContents.id = 'smiLayerContents';
+    myLayer.appendChild(myLayerContents);
+
+    var myLayerArrowB = document.createElement('div');
+    myLayerArrowB.id = 'smiLayerArrowB';
+    myLayer.appendChild(myLayerArrowB);
+
+    var myLayerArrowF = document.createElement('div');
+    myLayerArrowF.id = 'smiLayerArrowF';
+    myLayer.appendChild(myLayerArrowF);
+
+    myLayer.style.left = x;
+    myLayer.style.top = y;
+    return myLayer;
+}
+
+function loadOptions() {
+    var keys = [];  // 불러올 항목들의 이름
+
+    chrome.storage.local.get(keys, function (options) {
+        $.each(options, function (key, data) {
+
+        });
+    });
+}
+
+function getNextSMIText(rawData, posStart, posEnd)
+{
+    var str = rawData.substring(posStart, posEnd);
+    var posSyncStartTag = str.search("<SYNC");
+    if (posSyncStartTag == -1)
+        return null;
+    
+    var posABSSyncStartTag = posStart + posSyncStartTag;
+
+    str = str.substring(posSyncStartTag, str.length);
+    var posSyncEndTag = str.search(">");
+    if (posSyncEndTag == -1)
+        return null;
+
+    var posABSSyncEndTag = posABSSyncStartTag + posSyncEndTag;
+
+    str = str.substring(posSyncEndTag, str.length);
+    var posPStartTag = str.search("<P");
+    if (posPStartTag == -1)
+        return null;
+
+    var posABSPStartTag = posABSSyncEndTag + posPStartTag;
+
+    str = str.substring(posPStartTag, str.length);
+    var posPEndTag = str.search(">");
+    if (posPEndTag == -1)
+        return null;
+
+    var classRawStr = str.substring(posPStartTag, posPEndTag);
+    var posClassEqual = classRawStr.search("=");
+    var langType = classRawStr.substring(posClassEqual+1, posPEndTag);
+
+    var posABSPEndTag = posABSPStartTag + posPEndTag;
+
+    str = str.substring(posPEndTag, str.length);
+    var textLength = str.length;
+    var posNextSyncStartTag = str.search("<SYNC");
+    if (posNextSyncStartTag > -1)
+        textLength = posNextSyncStartTag;
+
+    var posABSNextSyncStartTag = posABSPEndTag + posNextSyncStartTag;
+
+    return { smiText: str.substring(1, textLength), posSyncStartTag: posABSSyncStartTag, posSyncEndTag: posABSSyncEndTag, posPStartTag: posABSPStartTag, posPEndTag: posABSPEndTag, posNextSyncStartTag: posABSNextSyncStartTag, langType: langType };
+}
