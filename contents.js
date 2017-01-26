d15f53b3c9d3d988f59ec4478dde3a428c46d185
yakolla <yakolla@gmail.com>
2017-01-26 17:31:20 +0900

    dd

--- contents.js
+++ contents.js
@@ -1,37 +1,192 @@
-﻿
-var layerCount = 0;
-var layers = {};
+﻿var debug = false;
+var layerUICount = 0;
+var layerUIs = {};
+var sliderUI;
+var allSyncTimes = [];
+var subtitleSyncTimes = {};
+var subtitleTexts = {};
+var allSyncTimeIdx = -1;
+
 $(document).ready(function () {
 
     loadOptions();
 
-    
-    var str = $("body pre").text();
+    Array.prototype.binaryIndexOf = binaryIndexOf;
 
-    var info = getNextSMIText(str, 0, str.length);    
+    var str = $("body pre").text();
+    var info = getNextSMIText(str, 0, str.length);
+    if (info == null)
+        return;
+
+    sliderUI = createSMISlider();
     lazyLoading(str, info);
+    
     $(document).mousedown(function (e) {
-        
+        //console.debug(subtitleSyncTimes);
         return true;
     });
 });
 
+function binaryIndexOf(searchElement) {
+    
+    var minIndex = 0;
+    var maxIndex = this.length - 1;
+    var currentIndex;
+    var currentElement;
+    var lowerIndex;
+
+    while (minIndex <= maxIndex) {
+        currentIndex = (minIndex + maxIndex) / 2 | 0;
+        currentElement = this[currentIndex];
+
+        if (currentElement < searchElement) {
+            minIndex = currentIndex + 1;
+        }
+        else if (currentElement > searchElement) {
+            maxIndex = currentIndex - 1;
+        }
+        else {
+            return currentIndex;
+        }
+    }
+    
+    lowerIndex = Math.abs(~minIndex) - 2;
+    
+    return lowerIndex;
+}
+
+
 function lazyLoading(str, info)
 {
+    if (info == null)
+        return;
+    
     setTimeout(function () {
-        if (info != null) {
-            var smiLayer = layers[info.langType];
-            if (null == smiLayer) {
-                smiLayer = createSMILayer(100, 250 * layerCount);
-                layers[info.langType] = smiLayer;
-                ++layerCount;
-            }
 
-            smiLayer.innerHTML += "<p>" + info.smiText + "</p>";
-            info = getNextSMIText(str, info.posNextSyncStartTag, str.length);
-            lazyLoading(str, info);
+        var syncTimes = subtitleSyncTimes[info.langType];
+        if (syncTimes == null)
+        {
+            subtitleSyncTimes[info.langType] = [];
+            subtitleTexts[info.langType] = {};
+
+            syncTimes = subtitleSyncTimes[info.langType];
         }
+
+        syncTimes.push(info.startTime);
+        allSyncTimes.push(info.startTime);
+
+        subtitleTexts[info.langType][info.startTime] = info.smiText;
+
+        if (debug == true)
+        {
+            var smiLayer = layerUIs[info.langType];
+            if (null == smiLayer) {
+                smiLayer = createSMILayer(100, 250 * layerUICount);
+                layerUIs[info.langType] = smiLayer;
+                ++layerUICount;
+            }
+            smiLayer.innerHTML += "<p>" + info.startTime + ":" + info.smiText + "</p>";
+        }
+        
+            
+        var lastSyncTime = syncTimes[syncTimes.length - 1];
+        if (sliderUI.smiSlider.max < lastSyncTime)
+            sliderUI.smiSlider.max = lastSyncTime;
+
+        // loading completed
+        if (info.posNextSyncStartTag == -1) {
+            allSyncTimes.sort(function (a, b) { return a - b; });
+            return;
+        }
+
+        info = getNextSMIText(str, info.posNextSyncStartTag, str.length);
+        lazyLoading(str, info);
+        
     }, 1);
+}
+
+function createSMISlider()
+{
+    var smiLayer = createSMILayer(600, 250 * layerUICount);
+
+    var myLayer = document.createElement('div');
+    document.body.appendChild(myLayer);
+    smiLayer.appendChild(myLayer);
+
+    var myPlayButton = document.createElement('button');
+    myPlayButton.id = 'smiPlayButton';
+    var myPlayText = document.createTextNode(">");
+    myPlayButton.appendChild(myPlayText);
+    myLayer.appendChild(myPlayButton);
+
+    var myPrevButton = document.createElement('button');
+    myPrevButton.id = 'smiPrevButton';
+    myPrevButton.appendChild(document.createTextNode("<-"));
+    myLayer.appendChild(myPrevButton);
+
+    var myNextButton = document.createElement('button');
+    myNextButton.id = 'smiNextButton';
+    myNextButton.appendChild(document.createTextNode("->"));
+    myLayer.appendChild(myNextButton);
+
+    var mySlider = document.createElement('input');
+    mySlider.setAttribute("type", "range");
+    mySlider.id = 'smiSlider';
+    mySlider.value = 0;
+    myLayer.appendChild(mySlider);
+
+
+    var myLayerContents = document.createElement('div');
+    myLayerContents.id = 'smiLayerContents';
+    myLayer.appendChild(myLayerContents);
+    var timerId = null;
+    myPlayButton.onclick = function () {
+        
+        if (timerId != null) {
+            clearInterval(timerId);
+            timerId = null;
+            return;
+        }
+
+        timerId = setInterval(function () {
+            myNextButton.onclick();            
+        }, 3000);
+       
+    };
+
+    myNextButton.onclick = function () {
+        if (allSyncTimeIdx == allSyncTimes.length - 1)
+            return;
+
+        ++allSyncTimeIdx;
+        mySlider.value = allSyncTimes[allSyncTimeIdx];
+        mySlider.onchange();        
+    };
+
+    myPrevButton.onclick = function () {
+        if (allSyncTimeIdx <= 0)
+            return;
+
+        --allSyncTimeIdx;
+        mySlider.value = allSyncTimes[allSyncTimeIdx];
+        mySlider.onchange();        
+    };
+
+    var layerContents = $("#smiLayerContents");
+    mySlider.onchange = function () {
+        allSyncTimeIdx = allSyncTimes.binaryIndexOf(this.value);
+        layerContents.html('');
+        for (var langType in subtitleSyncTimes) {
+            
+            var idx = subtitleSyncTimes[langType].binaryIndexOf(this.value);
+            if (idx > -1) {
+                var syncTime = subtitleSyncTimes[langType][idx];
+                layerContents.html(layerContents.html() + "<p>" + langType + ":" + syncTime + subtitleTexts[langType][syncTime] + "</p>");
+            }
+        }
+    };
+
+    return { smiLayer: smiLayer, smiSlider: mySlider };
 }
 
 function createSMILayer(x, y) {
@@ -39,10 +194,6 @@ function createSMILayer(x, y) {
     var myLayer = document.createElement('div');
     myLayer.id = 'smiLayer';
     document.body.appendChild(myLayer);
-
-    var myLayerContents = document.createElement('div');
-    myLayerContents.id = 'smiLayerContents';
-    myLayer.appendChild(myLayerContents);
 
     var myLayerArrowB = document.createElement('div');
     myLayerArrowB.id = 'smiLayerArrowB';
@@ -54,6 +205,7 @@ function createSMILayer(x, y) {
 
     myLayer.style.left = x;
     myLayer.style.top = y;
+
     return myLayer;
 }
 
@@ -67,47 +219,49 @@ function loadOptions() {
     });
 }
 
-function getNextSMIText(rawData, posStart, posEnd)
+function getSMIProperty(str, posStart, posEnd)
 {
-    var str = rawData.substring(posStart, posEnd);
-    var posSyncStartTag = str.search("<SYNC");
-    if (posSyncStartTag == -1)
+    var posEqual = str.indexOf("=", posStart);
+    return str.substring(posEqual + 1, posEnd);
+}
+
+function getSMITag(str, tag, posStart)
+{
+    var posABSStartTag = str.indexOf(tag, posStart);
+    if (posABSStartTag == -1)
         return null;
+
+    var posABSEndTag = str.indexOf(">", posABSStartTag);
+    if (posABSEndTag == -1)
+        return null;
+
+    return { posStart: posABSStartTag, posEnd: posABSEndTag };
+}
+
+function getNextSMIText(str, posStart, posEnd) {
+    var syncTag = getSMITag(str, "<SYNC", posStart);
+    if (syncTag == null)
+        return null;
+
+    var startTime = getSMIProperty(str, syncTag.posStart, syncTag.posEnd);
+
+    var pTag = getSMITag(str, "<P", syncTag.posEnd);
+    if (pTag == null)
+        return null;   
+
+    var langType = getSMIProperty(str, pTag.posStart, pTag.posEnd);
     
-    var posABSSyncStartTag = posStart + posSyncStartTag;
-
-    str = str.substring(posSyncStartTag, str.length);
-    var posSyncEndTag = str.search(">");
-    if (posSyncEndTag == -1)
-        return null;
-
-    var posABSSyncEndTag = posABSSyncStartTag + posSyncEndTag;
-
-    str = str.substring(posSyncEndTag, str.length);
-    var posPStartTag = str.search("<P");
-    if (posPStartTag == -1)
-        return null;
-
-    var posABSPStartTag = posABSSyncEndTag + posPStartTag;
-
-    str = str.substring(posPStartTag, str.length);
-    var posPEndTag = str.search(">");
-    if (posPEndTag == -1)
-        return null;
-
-    var classRawStr = str.substring(posPStartTag, posPEndTag);
-    var posClassEqual = classRawStr.search("=");
-    var langType = classRawStr.substring(posClassEqual+1, posPEndTag);
-
-    var posABSPEndTag = posABSPStartTag + posPEndTag;
-
-    str = str.substring(posPEndTag, str.length);
     var textLength = str.length;
-    var posNextSyncStartTag = str.search("<SYNC");
-    if (posNextSyncStartTag > -1)
-        textLength = posNextSyncStartTag;
+    var nextSyncTag = getSMITag(str, "<SYNC", pTag.posEnd);
+    if (nextSyncTag != null)
+        textLength = nextSyncTag.posStart;
+    else
+        nextSyncTag = { posStart: -1 };
 
-    var posABSNextSyncStartTag = posABSPEndTag + posNextSyncStartTag;
-
-    return { smiText: str.substring(1, textLength), posSyncStartTag: posABSSyncStartTag, posSyncEndTag: posABSSyncEndTag, posPStartTag: posABSPStartTag, posPEndTag: posABSPEndTag, posNextSyncStartTag: posABSNextSyncStartTag, langType: langType };
+    return {
+        smiText: str.substring(pTag.posEnd+1, textLength),
+        startTime: parseInt(startTime),
+        posNextSyncStartTag: parseInt(nextSyncTag.posStart),
+        langType: langType
+    };
 }
